b67be764e60c3c112cf72846158d7ef276d61feb
squidmin <morse.james.r@gmail.com>
2023-01-18 15:59:30 -0800

    Add: GCD; Fix indentation

---
+++ 07_recursion/recursion_5.py
@@ -0,0 +1,15 @@
+def main():
+    num1 = int(input('Enter an integer: '))
+    num2 = int(input('Enter another integer: '))
+
+    print('The greatest common divisor of the two numbers is', gcd(num1, num2))
+
+
+def gcd(x, y):
+    if x % y == 0:
+        return y
+    else:
+        return gcd(x, x % y)
+
+
+main()
