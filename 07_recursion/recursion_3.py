e227911d4b3a2e23cea728ad43c689a0437036e6
squidmin <morse.james.r@gmail.com>
2023-01-18 14:42:00 -0800

    Add: Recursion (cont'd)

---
+++ 07_recursion/recursion_3.py
@@ -0,0 +1,14 @@
+def main():
+  numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  sum = range_sum(numbers, 2, 5)
+  print('The sum of items 2 through 5 is ' + str(sum) + '.')
+
+
+def range_sum(num_list, start, end):
+  if start > end:
+    return 0
+  else:
+    return num_list[start] + range_sum(num_list, start + 1, end)
+
+
+main()
