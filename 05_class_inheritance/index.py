8d4257fbb45bd5d405f1ed86f3ba8df98ab0bbe2
squidmin <morse.james.r@gmail.com>
2023-01-18 13:49:25 -0800

    Add: Class inheritance: couple more classes

--- 05_class_inheritance/index.py
+++ 05_class_inheritance/index.py
@@ -2,12 +2,32 @@ import vehicles
 
 
 def main():
-  used_car = vehicles.Car('Audi', 2007, 12500, 2150.00, 4)
-  print('Make:', used_car.get_make())
-  print('Model:', used_car.get_model())
-  print('Mileage:', used_car.get_mileage())
-  print('Price:', used_car.get_price())
-  print('Number:', used_car.get_doors())
+  car = vehicles.Car('basic', 2007, 12500, 21500.00, 4)
+  truck = vehicles.Truck('python', 2002, 40000, 12000.0, '4WD')
+  suv = vehicles.SUV('classinheritance', 2000, 30000, 18500.0, 5)
 
+  print('INVENTORY')
+  print('=========')
+
+  print('Make: ', car.get_make())
+  print('Model: ', car.get_model())
+  print('Mileage: ', car.get_mileage())
+  print('Price: ', car.get_price())
+  print('Number of doors: ', car.get_doors())
+  print()
+
+  print('Make: ', truck.get_make())
+  print('Model: ', truck.get_model())
+  print('Mileage: ', truck.get_mileage())
+  print('Price: ', truck.get_price())
+  print('Drive type: ', truck.get_drive_type())
+  print()
+
+  print('Make: ', suv.get_make())
+  print('Model: ', suv.get_model())
+  print('Mileage: ', suv.get_mileage())
+  print('Price: ', suv.get_price())
+  print('Passenger capacity: ', suv.get_pass_cap())
+  print()
 
 main()
--- 05_class_inheritance/vehicles.py
+++ 05_class_inheritance/vehicles.py
@@ -40,4 +40,27 @@ class Car(Automobile):
 
   def get_doors(self):
     return self.__doors
+
+
+class Truck(Automobile):
+  def __init__(self, make, model, mileage, price, drive_type):
+    Automobile.__init__(self, make, model, mileage, price)
+    self.__drive_type = drive_type
+
+  def set_drive_type(self, drive_type):
+    self.__drive_type = drive_type
+
+  def get_drive_type(self):
+    return self.__drive_type
     
+
+class SUV(Automobile):
+  def __init__(self, make, model, mileage, price, pass_cap):
+    Automobile.__init__(self, make, model, mileage, price)
+    self.__pass_cap = pass_cap
+
+  def set_pass_cap(self, pass_cap):
+    self.__pass_cap = pass_cap
+
+  def get_pass_cap(self):
+    return self.__pass_cap
