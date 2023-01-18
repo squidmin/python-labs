88de5496f5990c85f057c162f59a0a4daeaef41b
squidmin <morse.james.r@gmail.com>
2023-01-18 14:32:58 -0800

    Add: Recursion: Vertical whitespace

--- 07_recursion/recursion_2.py
+++ 07_recursion/recursion_2.py
@@ -4,10 +4,12 @@ def main():
   fact = factorial(number)
   print('The factorial of', number, 'is', fact)
 
+
 def factorial(num):
   if num == 0:
     return 1
   else:
     return num * factorial(num - 1)
+
 
 main()
